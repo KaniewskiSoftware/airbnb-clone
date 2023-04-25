f750f0134b499249b8fd8420bd480e427ccc578f
KaniewskiSoftware <117393647+st4rkmano@users.noreply.github.com>
2023-04-25 20:28:05 +0200

    code formatted

--- app/components/modals/Modal.tsx
+++ app/components/modals/Modal.tsx
@@ -77,17 +77,17 @@ const Modal: React.FC<ModalProps> = ({
       >
         <div
           className="
-                relative
-                w-full
-                md:w-4/6
-                lg:w-3/6
-                xl:w-2/5
-                my-6
-                mx-auto
-                h-full
-                lg:h-auto
-                md:h-auto
-            "
+            relative
+            w-full
+            md:w-4/6
+            lg:w-3/6
+            xl:w-2/5
+            my-6
+            mx-auto
+            h-full
+            lg:h-auto
+            md:h-auto
+      "
         >
           {/*content*/}
           <div
