fad2579c3fc7d5d39a9137d32d136368ed9a7201
KaniewskiSoftware <117393647+st4rkmano@users.noreply.github.com>
2023-04-29 15:29:26 +0200

    Listings basic look added

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,40 @@
-import Image from 'next/image'
-import { Inter } from 'next/font/google'
+import getCurrentUser from "./actions/getCurrentUser";
+import getListings from "./actions/getListings";
+import Container from "./components/Container";
+import EmptyState from "./components/EmptyState";
+import ListingCard from "./components/listings/ListingCard";
 
-const inter = Inter({ subsets: ['latin'] })
+export default async function Home() {
+  const listings = await getListings();
+  const currentUser = await getCurrentUser();
 
-export default function Home() {
+  if (listings.length === 0) {
+    return <EmptyState showReset />;
+  }
+
   return (
-    <div className='text-rose-500 text-2xl'>Hello Airbnb</div>
-  )
+    <Container>
+      <div
+        className="
+        pt-24
+        grid
+        grid-cols-1
+        sm:grid-cols-2
+        md:grid-cols-3
+        lg:grid-cols-4
+        xl:grid-cols-5
+        2xl:grid-cols-6
+        gap-8
+      "
+      >
+        {listings.map((listing: any) => (
+          <ListingCard
+            key={listing.id}
+            currentUser={currentUser}
+            data={listing}
+          />
+        ))}
+      </div>
+    </Container>
+  );
 }
